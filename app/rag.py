fabf4f0995b2892a3b071403b0f838824125bee3
Jeevith Swarup <jeevithswaruptuta@gmail.com>
2026-07-08 10:35:11 +0530

    Add sentence-transformer embeddings for semantic search

--- app/rag.py
+++ app/rag.py
@@ -1,5 +1,22 @@
+from pypdf import PdfReader # type: ignore
+from langchain_text_splitters import RecursiveCharacterTextSplitter #type:ignore
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter #type:ignore
+
+# Reading the entire text from the PDF -----------------------------------------------------------
+
+def read_pdf(file_path:str)->str:
+    reader=PdfReader(file_path)
+
+    text=""
+
+    for page in reader.pages:
+        page_text=page.extract_text()       
+        if page_text:
+            text += page_text + "\n"
+
+    return text
+
+
 
 def create_chunks(text: str):
     splitter = RecursiveCharacterTextSplitter(
@@ -16,18 +33,5 @@ def create_chunks(text: str):
 
 
 
-# Reading the entire text from the PDF -----------------------------------------------------------
-# from pypdf import PdfReader # type: ignore
 
-# def read_pdf(file_path:str)->str:
-#     reader=PdfReader(file_path)
-
-#     text=""
-
-#     for page in reader.pages:
-        # page_text=page.extract_text()       
-#         if page_text:
-#             text += page_text + "\n"
-
-#     return text
                      
--- test.py
+++ test.py
@@ -1,5 +1,25 @@
-from app.rag import read_pdf
+from app.rag import read_pdf, create_chunks
+from app.embeddings import create_embedding
+text = read_pdf("documents/JeevithSwarup.pdf")
+
+
+
+
 
 text = read_pdf("documents/JeevithSwarup.pdf")
 
-print(text) 
+chunks = create_chunks(text)
+
+print(f"Total Chunks: {len(chunks)}")
+
+for i, chunk in enumerate(chunks):
+    print(f"\n===== Chunk {i+1} =====")
+    print(chunk)
+
+
+
+text = "Python is a programming language."
+
+embedding = create_embedding(text)
+
+print(embedding)    
